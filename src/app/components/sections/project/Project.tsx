625783eae0b81112f908c814990a8e0864cabd83
seungaaaaa <yg19877@naver.com>
2026-04-14 18:35:26 +0900

    프로젝트6개 추가

--- src/app/components/sections/project/Project.tsx
+++ src/app/components/sections/project/Project.tsx
@@ -8,23 +8,22 @@ const Project = () => {
 
     const mainProjects = [
         {
-            post: 'PC방',
-            title: 'pc-cafe',
-            desc: 'PC방 알바경험을 바탕으로 만든 PC방 관리 웹 서비스',
+            post: 'Perfume',
+            title: '향수 쇼핑몰',
+            desc: '고급스러운 메인페이지 디자인과 실제 구매 흐름을 구현한 향수 쇼핑몰',
             sub: [
-                'Local Storage + JSON 기반 주문·좌석·금액 데이터 관리',
-                '손님 페이지 ↔ 관리자 페이지 데이터 연계',
-                '좌석 번호 랜덤 생성 로직 구현',
-                '손님/관리자 페이지별 기능 구현 및 주문·좌석 상태 연동'
+                'Local Storage + JSON 기반 주문·장바구니·금액 데이터 관리',
+                '백엔드 없이 프론트엔드 환경에서 주문 로직 구현',
+                '공통 헤더·푸터를 인클루드 방식으로 구성해 서브페이지와 연동',
+                '실무를 고려한 폴더 구조로 서브페이지 구성'
             ],
             tech: [
                 '/stack/HTML.svg',
                 '/stack/CSS.svg',
                 '/stack/JavaScript.svg'
             ],
-            img: '/capture/pc.png',
-            link: 'https://seungaaaaa.github.io/pc/',
-            file: '/docs/기획서_김승아.pdf'
+            img: '/capture/perfume.png',
+            link: 'https://seungaa119.mycafe24.com/perfume/'
         },
         {
             post: '쇼핑몰',
@@ -44,24 +43,23 @@ const Project = () => {
             link: 'https://seungaaaaa.github.io/re01/',
         },
         {
-            post: '방명록',
-            title: '방명록',
-            desc: '방명록을 작성하면 서버에 저장되어 공유되는 웹 서비스',
+            post: 'PC방',
+            title: 'pc-cafe',
+            desc: 'PC방 알바경험을 바탕으로 만든 PC방 관리 웹 서비스',
             sub: [
-                'GCP (Clould SQL, Cloud Run)서비스',
-                'Node.js, MySQL(8.0)',
-                '비밀번호 기반 수정/삭제 제어 기능 구현',
-                '서버와 클라이언트 간 데이터 동기화 처리'
+                'Local Storage + JSON 기반 주문·좌석·금액 데이터 관리',
+                '손님 페이지 ↔ 관리자 페이지 데이터 연계',
+                '좌석 번호 랜덤 생성 로직 구현',
+                '손님/관리자 페이지별 기능 구현 및 주문·좌석 상태 연동'
             ],
             tech: [
-                '/stack/React-Dark.svg',
-                '/stack/NodeJS-Dark.svg',
-                '/stack/GCP-Dark.svg',
-                '/stack/MySQL-Dark.svg',
-                '/stack/dbeaver.png'
+                '/stack/HTML.svg',
+                '/stack/CSS.svg',
+                '/stack/JavaScript.svg'
             ],
-            img: '/capture/visited.png',
-            link: 'https://guestbook-205378419369.us-central1.run.app',
+            img: '/capture/pc.png',
+            link: 'https://seungaaaaa.github.io/pc/',
+            file: '/docs/기획서_김승아.pdf'
         },
         {
             post: '영화관',
@@ -104,10 +102,44 @@ const Project = () => {
         },
     ]
 
+    const work = [
+        {
+            src: '/capture/funize01.png',
+            link: 'https://seungaa119.mycafe24.com/funize02/',
+            title: '기존 기업 사이트 리뉴얼 (v1)'
+        },
+        {
+            src: '/capture/funize02.png',
+            link: 'https://seungaa119.mycafe24.com/funize04/',
+            title: '기존 기업 사이트 리뉴얼 (v2)'
+        },
+        {
+            src: '/capture/perfume2.png',
+            link: 'https://seungaa119.mycafe24.com/perfume/',
+            title: '향수 쇼핑몰'
+        },
+        {
+            src: '/capture/car1.png',
+            link: 'https://seungaa119.mycafe24.com/car01/',
+            title: '자동차 부품 기업 (메인 페이지 제작)'
+        },
+        {
+            src: '/capture/academy1.png',
+            link: 'https://seungaa119.mycafe24.com/academy02/',
+            title: '운전면허학원 (메인 페이지 제작)'
+        },
+        {
+            src: '/capture/jelly1.png',
+            link: 'https://seungaa119.mycafe24.com/jelly/',
+            title: '식품 브랜드 (메인 페이지 제작)'
+        }
+    ]
+
     const [select, setSelect] = useState(0)
     const [titleRef, titleView] = useView()
     const [titleRef2, titleView2] = useView()
     const [titleRef3, titleView3] = useView()
+    const [titleRef0, titleView0] = useView()
     const [oneRef, oneInView] = useView()
 
     return (
@@ -174,6 +206,23 @@ const Project = () => {
                         </div>
                     </article>
                 </main>
+
+                <div className={styles.others}>
+                <h2 className={`${styles.title} ${titleView0 ? styles.on : ''}`}>Responsive Web Projects<span className={styles.work_span}>프로젝트별 5일 내외로 메인 페이지 및 서브 페이지 구조 구현</span></h2>
+                <div className={styles.work} ref={titleRef0}>
+                    <div className={styles.workWrap}>
+                    {
+                        work.map((item, i) => (
+                                <div className={`${styles.canvasCard} ${styles.canvasCard}${i}`} key={i}>
+                                    <div className={styles.captureBox}><a href={item.link}><Image width={500} height={350} sizes='500px' className={styles.captureImg} src={item.src} alt={item.title} /></a></div>
+                                    <div>{item.title}</div>
+                                </div>
+                            ))
+                    }
+                    </div>                    
+                </div>
+                </div>
+
                 <div className={styles.others}>
                     <h2 className={`${styles.other_title} ${titleView2 ? styles.on : ''}`}>JavaScript Canvas Projects</h2>
                     <div className={styles.canvas} ref={titleRef2}>
